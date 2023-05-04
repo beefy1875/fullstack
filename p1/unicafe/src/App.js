bf17f3221d2f1b078203e0687ea1068d63c6de55
Sledger <beefy1875@hotmail.co.uk>
2023-05-04 11:07:58 +0800

    1.11 unicafe complete

---
+++ p1/unicafe/src/App.js
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.name}</button>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+
+  if (total === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={(good + bad) / total} />
+          <StatisticLine text="positive" value={`${good * 100 / total} %`} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => {
+    setGood(good + 1)
+  }
+
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const increaseBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={increaseGood} name="good" />
+      <Button handleClick={increaseNeutral} name="neutral" />
+      <Button handleClick={increaseBad} name="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
